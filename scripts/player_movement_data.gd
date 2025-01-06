c21b95f1cb7e0114888937d1be6d95419ce3eb81
lucbic64 <lucas.bichowicz@gmail.com>
2025-01-06 12:02:56 +0100

    Create data resource + adjust data values
    - created a script **player_movement_data.gd** as base for new resources
    - created a new resource of type PlayerMovementData called **DefaultPlayerMovement**
    - set values of movement data in **both the outdated player script and the new resource** to better base values

---
+++ scripts/player_movement_data.gd
@@ -0,0 +1,24 @@
+class_name PlayerMovementData
+extends Resource
+
+@export_group("Movement")
+@export_custom(PROPERTY_HINT_NONE, "suffix:px/s") var max_speed: float = 200
+@export_custom(PROPERTY_HINT_NONE, "suffix:px/s²") var acceleration: float = 50
+@export_custom(PROPERTY_HINT_NONE, "suffix:%") var friction: float = .2
+@export_custom(PROPERTY_HINT_NONE, "suffix:%") var air_friction: float = .05
+
+@export_group("Jump")
+@export_custom(PROPERTY_HINT_NONE, "suffix:px") var jump_height: float = 100
+## The time in seconds for the character to reach the jump's peak after the jump button was pressed 
+@export_custom(PROPERTY_HINT_NONE, "suffix:s") var jump_peak: float = 0.5
+## The time in seconds for the character to land on the ground after the peak of the jump was reached
+@export_custom(PROPERTY_HINT_NONE, "suffix:s") var jump_descent: float = 0.5
+
+var jump_velocity: float
+var jump_gravity: float
+var fall_gravity: float
+
+func _init() -> void:
+	jump_velocity = (2.0 * jump_height) / jump_peak * -1.0
+	jump_gravity = (2.0 * jump_height) / jump_peak ** 2
+	fall_gravity = (2.0 * jump_height) / jump_descent ** 2
