81228086237c9d9e6e947cf197bbbbd3c95859d0
lucbic64 <lucas.bichowicz@gmail.com>
2025-01-14 22:26:29 +0100

    Added warning ignore

--- scripts/events.gd
+++ scripts/events.gd
@@ -2,4 +2,6 @@ extends Node
 
 enum Pickups {APPLE, ELIXIR, COIN}
 
+# Warning due to the signal not being used directly in the class
+@warning_ignore("unused_signal")
 signal pickup_collected(type: Pickups)
