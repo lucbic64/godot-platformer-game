e007244e1b7f1e2f58271a708b960933d662cd7f
lucbic64 <lucas.bichowicz@gmail.com>
2025-01-14 21:51:21 +0100

    Add various underscores to stop warnings

--- scripts/states/run.gd
+++ scripts/states/run.gd
@@ -4,7 +4,7 @@ extends State
 @export var fall_state: State
 @export var jump_state: State
 
-func process_input(event: InputEvent) -> State:
+func process_input(_event: InputEvent) -> State:
 	if Input.is_action_just_pressed("jump") and parent.is_on_floor():
 		return jump_state
 	return null
