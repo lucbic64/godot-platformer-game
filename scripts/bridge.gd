e9c03ba8856afba90799e73eb93e4fceead551d7
lucbic64 <lucas.bichowicz@gmail.com>
2025-02-06 19:54:59 +0100

    Add bridge
    - added a "bridge" physics layer
    - created a bridge scene: it itself is just a Node2D for the position, and it consists of three frozen Rigidbody2D parts, with a sprite and a collision polygon each. In the scene, there is also the player detection component and a timer to time the total time till collapse (if bridge is set to collapse)
    - added a bridge.gd script: its a @tool so the color can be set in the inspector and to have certain exported variables be only exported if the bridge is set to "instable" using the property_list functionality. if the player is detected for the first time, the collapse timer is activated, where it is then paused or resumed based on whether the player is still standing on the bridge or not, or if he reentered the player detection area. the timers timeout signal is connected to the collapse function which, based on certain (conditionally) exported variables, makes the bridge collapse by unfreezing the rigidbody parts of the bridge.
    - added an instable bridge of color brown to the test_world, with a falling direction of right to left, due to the way the player will go over the brigde
    - locked a few nodes in the test_world so that the tilemaps arent selected constantly

---
+++ scripts/bridge.gd
@@ -0,0 +1,92 @@
+@tool
+extends Node2D
+
+@export_enum("Brown", "Yellow", "Red") var color: int: set = _set_color
+@export var instable: bool:
+	set(value):
+		instable = value
+		notify_property_list_changed()
+
+## The total time the player has to be on the bridge to make it collapse (Default: 0.2)
+var collapsing_time: float = 0.2 # exported if instable
+## The time delay after which the collapse continues with the next bridge part falling (Default: 0.2)
+var fall_delay: float = 0.2 # exported if instable
+## The order in which the bridge parts collapse (Default: Left-to-Right)
+var collapse_order: int # exported if instable
+
+@onready var player_detection = $PlayerDetection
+@onready var collapse_timer = $CollapseTimer
+@onready var left_part = $Left
+@onready var mid_part = $Mid
+@onready var right_part = $Right
+
+var _region_positions := [Vector2(144, 0), Vector2(144, 16), Vector2(144, 32)] # Brown, Yellow, Red
+
+var collapsed = false
+
+func _set_color(new_color: int) -> void:
+	color = new_color
+	
+	%BridgeSpriteLeft.region_rect.position = _region_positions[color]
+	%BridgeSpriteMid.region_rect.position = _region_positions[color] + Vector2(16, 0)
+	%BridgeSpriteRight.region_rect.position = _region_positions[color] + Vector2(32, 0)
+
+func _get_property_list() -> Array[Dictionary]:
+	if not Engine.is_editor_hint(): return []
+	
+	var ret: Array[Dictionary]
+	if instable:
+		ret.append({
+			"name": &"collapsing_time",
+			"class_name": &"",
+			"type": 3,
+			"hint": 0,
+			"hint_string": "",
+			"usage": 4102
+		})
+		
+		ret.append({
+			"name": &"fall_delay",
+			"class_name": &"",
+			"type": 3,
+			"hint": 0,
+			"hint_string": "",
+			"usage": 4102
+		})
+		
+		ret.append({
+			"name": &"collapse_order",
+			"class_name": &"",
+			"type": 2,
+			"hint": 2,
+			"hint_string": "Left-to-Right,Right-to-Left",
+			"usage": 4102
+		})
+	
+	return ret
+
+func _process(_delta: float) -> void:
+	if Engine.is_editor_hint() or not instable or collapsed: return
+	
+	if player_detection.has_overlapping_bodies():
+		if collapse_timer.is_stopped(): collapse_timer.start(collapsing_time)
+		
+		collapse_timer.paused = false
+	elif not collapse_timer.is_stopped():
+		collapse_timer.paused = true
+
+func collapse() -> void:
+	collapsed = true
+	
+	if collapse_order:
+		right_part.freeze = false
+		await get_tree().create_timer(fall_delay).timeout
+		mid_part.freeze = false
+		await get_tree().create_timer(fall_delay).timeout
+		left_part.freeze = false
+	else:
+		left_part.freeze = false
+		await get_tree().create_timer(fall_delay).timeout
+		mid_part.freeze = false
+		await get_tree().create_timer(fall_delay).timeout
+		right_part.freeze = false
